57a0c14142cf58e9ee01b5dcf8d5bd8ac64a4a6f
Valiantsin Lopan <valiantsin_lopan@live.com>
2017-02-13 14:12:37 +0300

    group by country added

---
+++ LINQtoXML/CustomerSorter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml;
+namespace LINQtoXML
+{
+    public class CustomerSorter
+    {
+        
+        public void SortCustomersByCountry()
+        {
+            XDocument xdoc = XDocument.Load(@"D:\VALIANTSIN\TAT LAB\RD. HW - AT Lab#. 05 - Customers.xml");
+            var groups =
+                from customer in xdoc.Root.Elements("customer")
+                group customer by new { country = (string)customer.Element("country") } into g
+                select new { g.Key,g};
+            foreach(var group in groups)
+            {
+                Console.WriteLine("Country: {0}",group.Key.country);
+                foreach (var customer in group.g)
+                {
+                    Console.WriteLine("\tCustomer Name: {0}; Customer ID: {1}", (string)customer.Element("name"), (string)customer.Element("id"));
+                }
+                Console.WriteLine();
+            }
+           // Console.ReadKey();    
+        }
+    }
+}
---
+++ LINQtoXMLTests/CustomerSorterTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LINQtoXML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQtoXML.Tests
+{
+    [TestClass()]
+    public class CustomerSorterTests
+    {
+        [TestMethod()]
+        public void SortCustomersByCountryTest()
+        {
+            var a = new CustomerSorter();
+            a.SortCustomersByCountry();
+            
+        }
+    }
+}
